7ab7d8078b7acfeb68817b90e711f606d5a4870c
Katelin Chan <kchan23@fb.com>
2019-06-26 15:28:55 -0700

    custom cells with title and synopsis

--- Flixster/View Controllers/MoviesViewController.m
+++ Flixster/View Controllers/MoviesViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MoviesViewController.h"
+#import "MovieCell.h"
 
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
 
@@ -56,10 +57,13 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    
+    MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
     
     NSDictionary *movie = self.movies[indexPath.row];
-    cell.textLabel.text = movie[@"title"];
+    cell.titleLabel.text = movie[@"title"];
+    cell.synopsisLabel.text = movie[@"overview"];
+//    cell.textLabel.text = movie[@"title"];
     
     return cell;
 }
---
+++ Flixster/Views/MovieCell.h
@@ -0,0 +1,20 @@
+//
+//  MovieCell.h
+//  Flixster
+//
+//  Created by kchan23 on 6/26/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
