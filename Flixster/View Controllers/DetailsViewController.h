ada00fede80543b37b26a64f2261be7cc061abf2
Katelin Chan <kchan23@fb.com>
2019-06-26 17:03:16 -0700

    segue to details page feature

---
+++ Flixster/View Controllers/DetailsViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailsViewController.h
+//  Flixster
+//
+//  Created by kchan23 on 6/26/19.
+//  Copyright © 2019 kchan23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property   (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
