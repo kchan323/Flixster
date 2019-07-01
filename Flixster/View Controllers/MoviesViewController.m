debaa33f3acdaa78e3e9f743914ddd6ad2d4a3dc
Katelin Chan <kchan23@fb.com>
2019-07-01 15:25:15 -0700

    autolayout

--- Flixster/View Controllers/MoviesViewController.m
+++ Flixster/View Controllers/MoviesViewController.m
@@ -105,6 +105,7 @@
 
     cell.titleLabel.text = movie[@"title"];
     cell.synopsisLabel.text = movie[@"overview"];
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
     
     NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
     NSString *posterURLString = movie[@"poster_path"];
